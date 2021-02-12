669cde222e72bf71369280a2a9290dd04cade38a
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-02-12 20:47:56 +0200

    task 1.2 logic done

--- Leonid.Tsigrinski_1.2_BUTTON.py
+++ Leonid.Tsigrinski_1.2_BUTTON.py
@@ -1,23 +1,49 @@
-# task 3
-# 7 segment led
-state = "0 - 9"
+# https://www.woolseyworkshop.com/2019/02/27/interfacing-a-7-segment-display-to-a-raspberry-pi/
+
+import time
+import RPi.GPIO as GPIO
+
+pin = [18, 21]
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, 0)
+time.sleep(1)
+
+
+def next_state():
+    global state, pin
+    if state == "from_start":
+        state = "from_end"
+
+    elif state == "from_end 0":
+        state = "random"
+
+    elif state == "random":
+        state = "from_start"
+
+
+GPIO.add_event_detect(button, GPIO.FALLING, callback=next_state, bouncetime=250)
+state = "from_start"
 number = 0  # current number
+
+
 while True:
-    if state == "0 - 9":
+    if state == "from_start":
         pass
         # show 0 - 9
-        # if button:
-        #   state = "9 - 0"
+        # number += 1
+        # if number = 9:
+        #   number = 0
+        # time.sleep(1)
 
 
-    elif state == "9 - 0":
+    elif state == "from_end":
         pass
         # change direction
-        # if button:
-        #   state = "random"
+        # time.sleep(1)
 
     elif state == "random":
         pass
         # randomm show
-        # if button:
-        #   state = "0 - 9" # RESTORE
+        # time.sleep(1)
