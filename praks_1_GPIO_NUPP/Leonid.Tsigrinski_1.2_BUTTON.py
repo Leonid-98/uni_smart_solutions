8bd4d4e4d1a56c20236998a6031b4fdc721b6ea4
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-02-14 19:56:15 +0200

    task 1.3 done logic

--- praks_1_GPIO_NUPP/Leonid.Tsigrinski_1.2_BUTTON.py
+++ praks_1_GPIO_NUPP/Leonid.Tsigrinski_1.2_BUTTON.py
@@ -1,6 +1,8 @@
 # https://www.woolseyworkshop.com/2019/02/27/interfacing-a-7-segment-display-to-a-raspberry-pi/
 
 import time
+from random import randrange
+
 import RPi.GPIO as GPIO
 
 pin = [18, 21]
@@ -10,6 +12,22 @@ GPIO.setwarnings(False)
 GPIO.setup(pin, GPIO.OUT)
 GPIO.output(pin, 0)
 time.sleep(1)
+
+
+def show_number(number):
+    d = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        0: []
+    }
+    GPIO.output(d[number], 1)
 
 
 def next_state():
@@ -28,23 +46,23 @@ GPIO.add_event_detect(button, GPIO.FALLING, callback=next_state, bouncetime=250)
 state = "from_start"
 number = 0  # current number
 
-
 while True:
     if state == "from_start":
-        pass
-        # show 0 - 9
-        # number += 1
-        # if number = 9:
-        #   number = 0
-        # time.sleep(1)
-
+        if number == 10: number = 0  # reset
+        show_number(number)
+        number += 1
+        time.sleep(0.5)
+        GPIO.output(pin, 0)
 
     elif state == "from_end":
-        pass
-        # change direction
-        # time.sleep(1)
+        if number == -1: number = 9  # reset
+        show_number(number)
+        number -= 1
+        time.sleep(0.5)
+        GPIO.output(pin, 0)
 
     elif state == "random":
-        pass
-        # randomm show
-        # time.sleep(1)
+        number = randrange(0, 10, 1)
+        show_number(number)
+        time.sleep(0.5)
+        GPIO.output(pin, 0)
---
+++ praks_1_GPIO_NUPP/test.py
@@ -0,0 +1,7 @@
+from random import randrange
+
+
+for i in range(100):
+    x = randrange(0, 10, 1)
+    if x == 9:
+        print(x)
