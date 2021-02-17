6be2779eefad7236c1dfff08bec955265a21c32f
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-02-17 19:31:21 +0200

    task 1.3 done logic

--- praks_2_SOCKETS/client.py
+++ praks_2_SOCKETS/client.py
@@ -1,40 +1,18 @@
 import socket
-import speech_recognition as sr
-import snowboydecoder
 
-HOST = "192.168.119.129"
-PORT = 12345
+ip = "127.0.0.1"
+port = 12345
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+client = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
 
-detector = snowboydecoder.HotwordDetector("computer.pmdl")
-sensitivity = 0.5
-print('Listening for hotword... PressCtrl+C to exit')
+client.connect(
+    (ip, port)
+)
 
-x = 0
-
-def speak():
-    global sr
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        print("Say something: ")
-        audio = r.listen(source)
-
-    try:
-        x = r.recognize_google(audio)
-        x = x.split(" ")
-        print(x)
-        x = str(x)
-        x = x.encode()
-        s.send(x)
-
-    except:
-        print("Couldn't detect audio")
-              
-detector.start(speak)
-detector.terminate()
-
-
+while True:
+    data = client.recv(2048)
+    print(data.decode("utf-8"))
 
