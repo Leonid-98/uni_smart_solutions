5351a0a0b6e4d9629365d30f19e719f7b6c296e2
leonid98@gmail.com <Leonid98@ut.ee>
2021-03-08 11:44:47 +0200

    12

--- praks_3_ARDUINO/3.2_UART/3.2_UART.ino
+++ praks_3_ARDUINO/3.2_UART/3.2_UART.ino
@@ -1,19 +1,41 @@
-String data;
+//
+//#include "U8glib.h"
+//U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);
+////U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE); 
+//
+//void setup(){
+//  u8g.setFont(u8g_font_unifont);
+//  u8g.setPrintPos(0, 20);}
+//
+//void loop() {
+//  u8g.firstPage();
+//  do {
+//    u8g.print("123");
+//    } while (u8g.nextPage());
+//  delay(500);
+//
+//  }
 
-void setup() {
-	Serial.begin(9600);
-	Serial.println("Arduino is ready: ");
-}
+//#include <SPI.h>
+//#include <Wire.h>
+//#include <Adafruit_SSD1306.h>
+// 
+//#define OLED_RESET 4
+//Adafruit_SSD1306 display(OLED_RESET);
+// 
+//void setup() {
+//  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+//}
+// 
+//void loop() {
+//  display.clearDisplay();
+//  display.setTextSize(2);
+//  display.setTextColor(WHITE);
+//  display.setCursor(0,0);
+//  display.print("123");
+//  display.display();
+//  delay(10000);
+//}
 
-void loop() {
-	while (Serial.available() > 0) {
-		char rec = Serial.read();
-		data += rec;
-		
-		if (rec == '\n') {
-			Serial.println(data);
-			data = "";
-		}
-	}
-	delay(10);
-}
+String nom = "Arduino";
+String msg
--- praks_3_ARDUINO/3.2_UART/3.2_UART.py
+++ praks_3_ARDUINO/3.2_UART/3.2_UART.py
@@ -6,20 +6,10 @@ import sys
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
 ser.flush()
 
-def accept_data():
-    global ser
     
-    while True:
-        data = ser.readlines().decode("utf-8")
-        if data:
-            print("<Serial rsponse>" + data)
-
-def send_data():
-    global ser
-    
-    while True:
-        data = input("::: ")
-        ser.write(("pi: " + data + "\n").encode())
+while True:
+    data = chr(input("::: "))
+    ser.write(data.encode())
 
 
 
