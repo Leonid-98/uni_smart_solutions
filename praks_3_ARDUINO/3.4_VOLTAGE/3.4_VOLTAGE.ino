894d9072fc3a905a96d4597c264aaaf78e0c465b
leonid98@gmail.com <Leonid98@ut.ee>
2021-03-22 11:16:19 +0200

    lab3 valmis

--- praks_3_ARDUINO/3.4_VOLTAGE/3.4_VOLTAGE.ino
+++ praks_3_ARDUINO/3.4_VOLTAGE/3.4_VOLTAGE.ino
@@ -1,10 +1,31 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET 7
+Adafruit_SSD1306 display(OLED_RESET);
+
+String data;
+
 void setup() {
   Serial.begin(9600);
-  }
+  Serial.println("Initial");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+}
 
-void loop(){
+void loop() {
   float sens = analogRead(A0);
-  Serial.println(sens * 5.0 / 1023.0);
+  float voltage = sens * 5.0 / 1023.0;
+  showData(voltage);
+   
   delay(50);
-  
+}
+
+void showData(float data){
+    Serial.println(data);
+    Serial.println();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print(data);
+    display.display();
   }
